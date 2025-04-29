2199b343f333aebe67cb8fdb3945d1a6731a9758
kelter-js <spawnkillerqw@gmail.com>
2025-04-29 15:04:40 +0500

    feat: experimenting with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,19 @@
-export const App = () => <div>Hello world!</div>;
+import { useEffect } from "react";
+import { Map } from "./components/Map";
+import { GameState } from "./types";
+import { useGameSaves } from "./stores/GameSave";
+import { SaveList } from "./components/SaveList";
+import { SnackbarProvider } from "./contexts/Snackbar";
+
+export const App = () => {
+  const { gameSaves } = useGameSaves();
+  console.log(gameSaves);
+
+  // return <ImageMapHighlight />;
+  return (
+    <SnackbarProvider>
+      <Map />
+      <SaveList isLoadMode />
+    </SnackbarProvider>
+  );
+};
