b2ed667f1160ef03c5d268e3092b71295df6ada4
kelter-js <spawnkillerqw@gmail.com>
2025-10-31 17:14:42 +0500

    feat: реализация экономических направлений в UI

--- src/components/EconomicCard/EconomicCard.styled.tsx
+++ src/components/EconomicCard/EconomicCard.styled.tsx
@@ -1,34 +1,40 @@
-import { styled, Typography } from "@mui/material";
+import { styled } from "@mui/material";
+import { QuestCardText } from "../../common/styled.index";
 
 export const QuestCardContainer = styled("div")`
   position: relative;
   transition: transform 0.3s ease;
   cursor: pointer;
+
   & img {
     border-radius: 25px;
   }
+
   &:hover {
     transform: scale(1.05);
   }
 `;
 
-export const QuestCardTitle = styled(Typography)`
+export const QuestCardTitle = styled(QuestCardText)`
   position: absolute;
   top: 2px;
   left: 50%;
   transform: translateX(-50%);
 `;
 
-export const QuestCardDescription = styled(Typography)`
+export const QuestCardDescription = styled(QuestCardText)`
   position: absolute;
   top: 425px;
   left: 50%;
   transform: translateX(-50%);
 `;
 
-export const QuestCardReward = styled(Typography)`
-  position: absolute;
-  bottom: 1px;
-  left: 50%;
-  transform: translateX(-50%);
-`;
+export const QuestCardReward = styled(QuestCardText)(
+  ({ theme: { spacing } }) => ({
+    position: "absolute",
+    bottom: "1px",
+    left: "50%",
+    transform: "translateX(-50%)",
+    padding: spacing(2),
+  })
+);
--- src/components/EconomicCard/EconomicCard.tsx
+++ src/components/EconomicCard/EconomicCard.tsx
@@ -5,6 +5,8 @@ import { getEconomicBranchDataByType } from "./utils";
 import { EconomicCardProps } from "./types";
 import * as S from "./EconomicCard.styled";
 
+// REFACTORING CHECKED ✅
+
 export const EconomicCard: FC<EconomicCardProps> = ({ type }) => {
   const { setEconomicBranch } = useGameState();
   const { toggleEconomicModal } = useAppState();
@@ -19,53 +21,12 @@ export const EconomicCard: FC<EconomicCardProps> = ({ type }) => {
   return (
     <S.QuestCardContainer onClick={handleSelectEconomicBranch}>
       <img src={src} width="350px" height="620px" />
-      <S.QuestCardTitle
-        sx={{
-          width: "100%",
-          color: "black",
-          fontWeight: "bold",
-          textTransform: "uppercase",
-          letterSpacing: "1px",
-          p: "8px",
-          fontSize: "20px",
-          fontFamily: "inherit",
-          textAlign: "center",
-        }}
-      >
-        {title}
-      </S.QuestCardTitle>
 
-      <S.QuestCardDescription
-        sx={{
-          width: "90%",
-          color: "black",
-          fontWeight: "bold",
-          textTransform: "uppercase",
-          letterSpacing: "1px",
-          p: "8px",
-          fontSize: "18px",
-          fontFamily: "inherit",
-          textAlign: "center",
-        }}
-      >
-        {description}
-      </S.QuestCardDescription>
+      <S.QuestCardTitle>{title}</S.QuestCardTitle>
 
-      <S.QuestCardReward
-        sx={{
-          width: "100%",
-          color: "black",
-          fontWeight: "bold",
-          textTransform: "uppercase",
-          letterSpacing: "1px",
-          padding: (theme) => theme.spacing(2),
-          fontSize: "20px",
-          fontFamily: "inherit",
-          textAlign: "center",
-        }}
-      >
-        {reward.join(", ")}
-      </S.QuestCardReward>
+      <S.QuestCardDescription>{description}</S.QuestCardDescription>
+
+      <S.QuestCardReward>{reward.join(", ")}</S.QuestCardReward>
     </S.QuestCardContainer>
   );
 };
